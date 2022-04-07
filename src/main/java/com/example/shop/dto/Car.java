d360a5b40c4dbf06b7299079e9bb9a650651b9b9
amilbek <amilbek.nurbolat@gmail.com>
2022-04-07 20:24:39 +0600

    remove builders

--- src/main/java/com/example/shop/dto/Car.java
+++ src/main/java/com/example/shop/dto/Car.java
@@ -18,55 +18,6 @@ public class Car {
     public Car() {
     }
 
-    public static CarBuilder carBuilder() {
-        return new Car().new CarBuilder();
-    }
-
-    public class CarBuilder {
-
-        private CarBuilder() {
-        }
-
-        public CarBuilder carNumber(Long carNumber) {
-            Car.this.carNumber = carNumber;
-            return this;
-        }
-
-        public CarBuilder brand(String brand) {
-            Car.this.brand = brand;
-            return this;
-        }
-
-        public CarBuilder model(String model) {
-            Car.this.model = model;
-            return this;
-        }
-
-        public CarBuilder color(String color) {
-            Car.this.color = color;
-            return this;
-        }
-
-        public CarBuilder year(Year date) {
-            Car.this.productionYear = date;
-            return this;
-        }
-
-        public CarBuilder price(double price) {
-            Car.this.price = price;
-            return this;
-        }
-
-        public CarBuilder availability(boolean availability) {
-            Car.this.isAvailable = availability;
-            return this;
-        }
-
-        public Car build() {
-            return Car.this;
-        }
-    }
-
     @Override
     public String toString() {
         return "Car{" +
